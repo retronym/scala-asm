scala/scala-asm-legacy

[asm-cherry-pick] Allow setting stack values in analysis frames
Adds a method `setStack` to analysis.Frame which allows setting the
dataflow value of a stack slot.

Used in nullness analysis. After, for example, an instance call,
aliases of the receiver can be set to NotNull, both in locals and
on the stack.

src/main/java/scala/tools/asm/tree/analysis/Frame.java
@@ -203,6 +203,20 @@ public class Frame<V extends Value> {
     }
 
     /**
+     * Sets the value of the given stack slot.
+     *
+     * @param i
+     *   the index of an operand stack slot.
+     * @param value
+     *   the new value of the stack slot.
+     * @throws IndexOutOfBoundsException
+     *   if the stack slot does not exist.
+     */
+    public void setStack(final int i, final V value) throws IndexOutOfBoundsException {
+        values[i + locals] = value;
+    }
+
+    /**
      * Clears the operand stack of this frame.
      */
     public void clearStack() {

retronym/scala-asm

src/main/java/scala/tools/asm/tree/analysis/Frame.java
@@ -179,7 +179,21 @@ public class Frame<V extends Value> {
     return values[nLocals + index];
   }
 
-  /** Clears the operand stack of this frame. */
+  /**
+   * Sets the value of the given stack slot.
+   *
+   * @param i
+   *   the index of an operand stack slot.
+   * @param value
+   *   the new value of the stack slot.
+   * @throws IndexOutOfBoundsException
+   *   if the stack slot does not exist.
+   */
+  public void setStack(final int i, final V value) throws IndexOutOfBoundsException {
+      values[i + nLocals] = value;
+  }
+
+    /** Clears the operand stack of this frame. */
   public void clearStack() {
     nStack = 0;
   }